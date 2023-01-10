cd5b4e6f0cd2d3f396c96e3731a471b9e6b7cb8b
hakanmarthidir <hakan_hidir@hotmail.com>
2023-01-10 23:12:53 +0100

    provider test middleware solution

--- producer.test/ProviderStartup.cs
+++ producer.test/ProviderStartup.cs
@@ -2,31 +2,37 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
-using Microsoft.AspNetCore.Http;
-using producer.Controllers;
 
 namespace producer.test
 {
     public class ProviderStartup
     {              
         public ProviderStartup(IConfiguration configuration)
-        {            
+        {
+            //Customize if you need
         }
         public void ConfigureServices(IServiceCollection services)
-        {            
+        {          
+            //Customize if you need
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseRouting();            
+            //Method 1 - Solution with Middleware
+            app.UseMiddleware<ProviderStateMiddleware>();
 
-            app.UseEndpoints(endpoints =>
-            {
-                List<Category> categories = new List<Category>() {
-                    new Category() { Id = new Guid("62380d5e-2554-49c9-a814-13ab1f50d73e"), Name = "category1", ProductCount = 3 },
-                    new Category() { Id = new Guid("1c319a81-22b6-4971-b774-4963db5099db"), Name = "category2", ProductCount = 5} };
-                endpoints.MapGet("/api/v1/category", () => { return Results.Ok(categories); });
-            });
+
+
+            //Method 2 - or you can use the MinimalAPI
+
+            //app.UseRouting();
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    List<Category> categories = new List<Category>() {
+            //        new Category() { Id = new Guid("62380d5e-2554-49c9-a814-13ab1f50d73e"), Name = "category1", ProductCount = 3 },
+            //        new Category() { Id = new Guid("1c319a81-22b6-4971-b774-4963db5099db"), Name = "category2", ProductCount = 5} };
+            //    endpoints.MapGet("/api/v1/category", () => { return Results.Ok(categories); });
+            //});
         }
     }
 
---
+++ producer.test/ProviderStateMiddleware.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using producer.Controllers;
+using System.Net;
+
+namespace producer.test
+{
+
+    public class ProviderStateMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public ProviderStateMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            context.Response.ContentType = "application/json; charset=utf-8";
+            context.Response.StatusCode = (int)HttpStatusCode.OK;
+
+            if (context.Request.Method == HttpMethod.Get.ToString())
+            {
+                if (context.Request.Path.Value == "/api/v1/category")
+                {
+                    List<Category> categories = new List<Category>() {
+                    new Category() { Id = new Guid("62380d5e-2554-49c9-a814-13ab1f50d73e"), Name = "category1", ProductCount = 3 },
+                    new Category() { Id = new Guid("1c319a81-22b6-4971-b774-4963db5099db"), Name = "category2", ProductCount = 5} };
+
+                    DefaultContractResolver contractResolver = new DefaultContractResolver
+                    {
+                        NamingStrategy = new CamelCaseNamingStrategy()
+                    };
+
+                    var serialized = JsonConvert.SerializeObject(categories, new JsonSerializerSettings
+                    {
+                        ContractResolver = contractResolver,
+                        Formatting = Formatting.Indented
+                    });
+                    await context.Response.WriteAsync(serialized);
+                    return;
+                }
+                else
+                {
+                    List<Category> categories = new List<Category>();
+                    var serialized = JsonConvert.SerializeObject(categories);
+                    await context.Response.WriteAsync(serialized);
+                    return;
+                }
+            }
+
+            await this._next(context);
+        }
+    }
+}
